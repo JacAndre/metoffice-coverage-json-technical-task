f067176c857a19817723b9078c94a2de8b467aed
Jacques Andre <jacques_andre@ymail.com>
2025-10-03 21:56:50 +0100

    Added main method to run application

--- src/main/java/com/jacandre/App.java
+++ src/main/java/com/jacandre/App.java
@@ -1,13 +1,33 @@
 package com.jacandre;
 
-/**
- * Hello world!
- *
- */
+import com.jacandre.coverage.mapper.CoverageMapper;
+import com.jacandre.model.WeatherRecord;
+import com.jacandre.utils.WeatherCSVReader;
+import com.jacandre.utils.WeatherCSVValidator;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.jacandre.utils.CoverageJsonWriter.toJson;
+
+@Slf4j
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws Exception {
+        WeatherCSVReader reader = new WeatherCSVReader();
+        WeatherCSVValidator validator = new WeatherCSVValidator();
+        List<WeatherRecord> records = new ArrayList<>();
+
+        var parsedCsv = reader.readFromFile("data/weather.csv");
+        for (String[] row : parsedCsv) {
+            if (!validator.validateParsedRow(row)) return;
+            records.add(WeatherRecord.parse(row));
+        }
+
+        CoverageMapper mapper = new CoverageMapper();
+        var coverage = mapper.map(records);
+
+        log.info("{}", toJson(coverage));
     }
 }
