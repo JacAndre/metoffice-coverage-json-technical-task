19bcbab1f309d1ead542b894a5fc738c72be5e13
Jacques Andre <jacques_andre@ymail.com>
2025-10-04 12:26:34 +0100

    Updated error handling on toJson

--- src/main/java/com/jacandre/utils/CoverageJsonWriter.java
+++ src/main/java/com/jacandre/utils/CoverageJsonWriter.java
@@ -1,13 +1,21 @@
 package com.jacandre.utils;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.jacandre.coverage.model.Coverage;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class CoverageJsonWriter {
     private static final ObjectMapper mapper = new ObjectMapper().enable(SerializationFeature.INDENT_OUTPUT);
 
-    public static String toJson(Coverage coverage) throws Exception {
-        return mapper.writeValueAsString(coverage);
+    public static String toJson(Coverage coverage) {
+        try {
+            return mapper.writeValueAsString(coverage);
+        } catch (JsonProcessingException e) {
+            log.error("CoverageJsonWriter::toJson - Failed to serialize Coverage", e);
+            return "{}";
+        }
     }
 }
