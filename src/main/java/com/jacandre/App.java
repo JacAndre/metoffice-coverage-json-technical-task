91e0ea7265b75d878d2ac60bd6839e03e940e21c
Jacques Andre <jacques_andre@ymail.com>
2025-10-04 10:48:36 +0100

    Refactoring and cleaning up App

--- src/main/java/com/jacandre/App.java
+++ src/main/java/com/jacandre/App.java
@@ -2,14 +2,13 @@ package com.jacandre;
 
 import com.jacandre.coverage.mapper.CoverageMapper;
 import com.jacandre.model.WeatherRecord;
+import com.jacandre.utils.CoverageJsonExporter;
 import com.jacandre.utils.WeatherCSVReader;
 import com.jacandre.utils.WeatherCSVValidator;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import static com.jacandre.utils.CoverageJsonWriter.toJson;
 
 @Slf4j
 public class App 
@@ -19,15 +18,19 @@ public class App
         WeatherCSVValidator validator = new WeatherCSVValidator();
         List<WeatherRecord> records = new ArrayList<>();
 
-        var parsedCsv = reader.readFromFile("data/weather.csv");
+        var parsedCsv = reader.readFromFile("data/input/weather.csv");
         for (String[] row : parsedCsv) {
-            if (!validator.validateParsedRow(row)) return;
+            if (!validator.validateParsedRow(row)) {
+                log.warn("Skipping invalid row: {}", (Object) row);
+                continue;
+            }
             records.add(WeatherRecord.parse(row));
         }
 
         CoverageMapper mapper = new CoverageMapper();
         var coverage = mapper.map(records);
 
-        log.info("{}", toJson(coverage));
+        CoverageJsonExporter exporter = new CoverageJsonExporter();
+        exporter.exportToFile(coverage);
     }
 }
