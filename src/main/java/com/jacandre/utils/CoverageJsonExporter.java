5e1f2f6180f9658d86961c0f62ddbb1e6dc5c4ce
Jacques Andre <jacques_andre@ymail.com>
2025-10-04 08:59:49 +0100

    Created new util for exporting result to Json

---
+++ src/main/java/com/jacandre/utils/CoverageJsonExporter.java
@@ -0,0 +1,30 @@
+package com.jacandre.utils;
+
+import com.jacandre.coverage.model.Coverage;
+import lombok.extern.slf4j.Slf4j;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Slf4j
+public class CoverageJsonExporter {
+
+    public void exportToFile(Coverage coverage) throws Exception {
+        try {
+            LocalDateTime now = LocalDateTime.now();
+            String timeStamp = now.format(DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss"));
+
+            Path outputDir = Path.of("data/output");
+            Files.createDirectories(outputDir);
+
+            Path outputFile = outputDir.resolve("coverage_" + timeStamp + ".json");
+            Files.writeString(outputFile, CoverageJsonWriter.toJson(coverage));
+
+            log.info("CoverageJSON written to {}", outputFile.toAbsolutePath());
+        } catch (Exception e) {
+            log.error("CoverageJsonExporter::exportToFile - Failed to export CoverageJSON", e);
+        }
+    }
+}
